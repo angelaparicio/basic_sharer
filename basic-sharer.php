0116fe552da3d0032e7ef56b1ced58909bb66d58
Angel <a.l.aparicio.gomez@gmail.com>
2017-05-25 14:28:04 +0200

    Eliminados alt de los enlaces por compatibilidad con AMP

--- basic-sharer.php
+++ basic-sharer.php
@@ -16,16 +16,16 @@ function basic_sharer_icons($content) {
 	
 	$shares .= '<strong> Compartir:</strong> <br/>';
 	
-	$shares .= '<a href="http://www.facebook.com/sharer.php?u=' . apply_filters("the_permalink", get_permalink()) . '&t=' . urlencode(get_the_title()) . '" target="_blank" style="position: relative; top: 0.15em;" alt="Compartir en Facebook" title="Compartir en Facebook">';
+	$shares .= '<a href="http://www.facebook.com/sharer.php?u=' . apply_filters("the_permalink", get_permalink()) . '&t=' . urlencode(get_the_title()) . '" target="_blank" style="position: relative; top: 0.15em;" title="Compartir en Facebook">';
 	$shares .= '<img src="'.get_site_url().'/wp-content/plugins/basic-sharer/images/fb-24.png" alt="Facebook" /> </a>';
 
-	$shares .= '<a href="http://twitter.com/share?text=' . urlencode(get_the_title()) . '&url=' . apply_filters("the_permalink", get_permalink()) . '&via=' . get_option('blogname') . '" target="_blank" style="position: relative; top: 0.15em;" alt="Compartir en Twitter" title="Compartir en Twitter">';
+	$shares .= '<a href="http://twitter.com/share?text=' . urlencode(get_the_title()) . '&url=' . apply_filters("the_permalink", get_permalink()) . '&via=' . get_option('blogname') . '" target="_blank" style="position: relative; top: 0.15em;" title="Compartir en Twitter">';
 	$shares .= '<img src="'.get_site_url().'/wp-content/plugins/basic-sharer/images/tw-24.png" alt="Twitter" /> </a>';	
 
-	$shares .= '<a href="https://www.linkedin.com/shareArticle?mini=true&title=' . urlencode(get_the_title()) . '&url=' . apply_filters("the_permalink", get_permalink()) . '" target="_blank" style="position: relative; top: 0.15em;" alt="Compartir en Twitter" title="Compartir en Twitter">';
+	$shares .= '<a href="https://www.linkedin.com/shareArticle?mini=true&title=' . urlencode(get_the_title()) . '&url=' . apply_filters("the_permalink", get_permalink()) . '" target="_blank" style="position: relative; top: 0.15em;" title="Compartir en Twitter">';
 	$shares .= '<img src="'.get_site_url().'/wp-content/plugins/basic-sharer/images/ln-24.png" alt="Twitter" /> </a>';	
 
-	$shares .= '<a href="https://plus.google.com/share?url=' . apply_filters("the_permalink", get_permalink()) . '" target="_blank" style="position: relative; top: 0.15em;" alt="Compartir en Twitter" title="Compartir en Twitter">';
+	$shares .= '<a href="https://plus.google.com/share?url=' . apply_filters("the_permalink", get_permalink()) . '" target="_blank" style="position: relative; top: 0.15em;" title="Compartir en Twitter">';
 	$shares .= '<img src="'.get_site_url().'/wp-content/plugins/basic-sharer/images/gp-24.png" alt="Twitter" /> </a>';
 
 	$shares .= '</div>';
